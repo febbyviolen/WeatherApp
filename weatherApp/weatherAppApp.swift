22b81821fec2b90d26b678db3f31d48fb71739b8
febbyviolen <febbyviolen@gmail.com>
2022-08-14 22:50:43 +0900

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Ebbyy on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
